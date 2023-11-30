9aa28d335a9447238a02a4dfec5f0adb846d6ecb
baserak <almaz.talduk@gmail.com>
2023-11-30 18:56:40 +0600

    Update start_linux.sh
    Clear cmd line

--- start_linux.sh
+++ start_linux.sh
@@ -62,6 +62,6 @@ export CUDA_HOME="$CUDA_PATH"
 # activate installer env
 source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
 conda activate "$INSTALL_ENV_DIR"
-
+clear
 # setup installer env
 python one_click.py $@
